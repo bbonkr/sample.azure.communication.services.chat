5870a9f6f5359bda31d1dd068b484a4a70a70832
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-05-20 15:18:58 +0900

    Add Users controller

--- src/Sample.Chat/Controllers/UsersController.cs
+++ src/Sample.Chat/Controllers/UsersController.cs
@@ -8,6 +8,9 @@ using kr.bbon.AspNetCore.Filters;
 
 using Microsoft.AspNetCore.Mvc;
 using kr.bbon.AspNetCore;
+using Sample.Chat.Services;
+using System.Net;
+using Sample.Chat.Services.Models;
 
 namespace Sample.Chat.Controllers
 {
@@ -18,6 +21,76 @@ namespace Sample.Chat.Controllers
     [ApiVersion("1.0")]
     public class UsersController : ApiControllerBase
     {
-        
+        public UsersController(
+            IUserService userService,
+            IChatService chatService)
+        {
+            this.userService = userService;
+            this.chatService = chatService;
+        }
+
+        [HttpGet]
+        [Route("{email}")]
+        public async Task<IActionResult> LoginAsync(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return StatusCode(HttpStatusCode.BadRequest, "Email address is invalid.");
+            }
+
+            var result = await userService.GetUserAsync(new GetUserRequestModel { Email = email });
+
+            return StatusCode(HttpStatusCode.OK, result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegistAsync(CreateUserRequestModel model)
+        {
+            model.Email = model.Email?.Trim();
+            model.DisplayName = model.DisplayName?.Trim();
+
+            if (string.IsNullOrWhiteSpace(model.Email))
+            {
+                return StatusCode(HttpStatusCode.BadRequest, "Email address is invalid.");
+            }
+
+            if (string.IsNullOrWhiteSpace(model.DisplayName))
+            {
+                return StatusCode(HttpStatusCode.BadRequest, "Display name is invalid.");
+            }
+
+            var result = await userService.CreateUserAsync(model);
+
+            return StatusCode(HttpStatusCode.Created, result);
+        }
+
+        [HttpDelete]
+        [Route("{email}")]
+        public async Task<IActionResult> UnregistAsync(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return StatusCode(HttpStatusCode.BadRequest, "Email address is invalid.");
+            }
+
+            // Withdraw from all thread.
+            await chatService.WithdrawFromAllThread(email);
+
+            var result = await userService.DeleteAsync(new GetUserRequestModel
+            {
+                Email = email,
+            });
+
+            if (result == 0)
+            {
+                return StatusCode(HttpStatusCode.BadRequest, "Email address is invalid.");
+            }
+
+            return StatusCode(HttpStatusCode.OK, "Done.");
+        }
+
+
+        private readonly IUserService userService;
+        private readonly IChatService chatService;
     }
 }
